efe30edfd787fa9f7e93109f2033dd316f8d7661
Yuriy Bogdanov <chinsay@gmail.com>
2015-11-23 19:52:00 +0200

    rocker-compose 0.1.2

--- rocker-compose.rb
+++ rocker-compose.rb
@@ -1,15 +1,15 @@
 class RockerCompose < Formula
   desc "Docker tool for deploying apps composed of multiple containers."
   homepage "https://github.com/grammarly/rocker-compose"
-  url "https://github.com/grammarly/rocker-compose.git", :tag => "0.1.1"
+  url "https://github.com/grammarly/rocker-compose.git", :tag => "0.1.2"
   head "https://github.com/grammarly/rocker-compose.git"
-  version "0.1.1"
+  version "0.1.2"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = "#{Dir.pwd}/vendor:#{Dir.pwd}"
-    system "make", "local-binary"
+    system "make", "binary"
     bin.install "bin/rocker-compose"
   end
 
