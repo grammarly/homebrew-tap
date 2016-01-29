cac004d0fc4b835f0006e5f3f5581da00128cf97
Yuriy Bogdanov <chinsay@gmail.com>
2016-01-29 17:09:20 +0200

    rocker: correctly set version to a binary

--- rocker.rb
+++ rocker.rb
@@ -10,9 +10,15 @@ class Rocker < Formula
   def install
     ENV["GOPATH"] = "#{Dir.pwd}/build"
     ENV["GO15VENDOREXPERIMENT"] = "1"
+    sha = `git rev-parse HEAD 2>/dev/null`
+    t = DateTime.now.new_offset(0).strftime("%Y-%m-%d_%H:%M_GMT")
     system "mkdir -p build/src/github.com/grammarly"
     system "ln -s `pwd` build/src/github.com/grammarly/rocker"
-    system "cd ./build/src/github.com/grammarly/rocker && go install "
+    system [
+      "cd ./build/src/github.com/grammarly/rocker && go install",
+      "-ldflags \"-X main.Version=#{version} -X main.GitCommit=#{sha[0,7]}",
+      "-X main.GitBranch=master -X main.BuildTime=#{t}_Homebrew\""
+    ].join(" ")
     bin.install "build/bin/rocker"
   end
 
