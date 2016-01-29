262ff4fbf50281bcf0bcf20ce29cf0ddeeec20ba
Roman Khlystik <roman.khlystik@gmail.com>
2016-01-29 16:03:11 +0200

    rocker 1.1.0

--- rocker.rb
+++ rocker.rb
@@ -1,19 +1,19 @@
 class Rocker < Formula
   desc "Rocker breaks the limits of Dockerfile."
   homepage "https://github.com/grammarly/rocker"
-  url "https://github.com/grammarly/rocker.git", :tag => "1.0.1"
+  url "https://github.com/grammarly/rocker.git", :tag => "1.1.0"
   head "https://github.com/grammarly/rocker.git"
-  version "1.0.1"
+  version "1.1.0"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = "#{Dir.pwd}/vendor:#{Dir.pwd}"
-    system "make", "binary"
-    bin.install "bin/rocker"
+    system "make", "default"
+    bin.install "./rocker"
   end
 
   test do
-    shell "#{bin}/rocker --version"
+    shell "./rocker --version"
   end
 end
