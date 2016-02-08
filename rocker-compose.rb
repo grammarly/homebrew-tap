2b86820090cfcb6769044f7d11b16ccfda207000
Roman Khlystik <roman.khlystik@gmail.com>
2016-02-08 14:55:44 +0200

    Rocker-compose updated to 0.1.4. Changed build

--- rocker-compose.rb
+++ rocker-compose.rb
@@ -1,16 +1,26 @@
 class RockerCompose < Formula
   desc "Docker tool for deploying apps composed of multiple containers."
   homepage "https://github.com/grammarly/rocker-compose"
-  url "https://github.com/grammarly/rocker-compose.git", :tag => "0.1.3"
+  url "https://github.com/grammarly/rocker-compose.git", :tag => "0.1.4"
   head "https://github.com/grammarly/rocker-compose.git"
-  version "0.1.3"
+  version "0.1.4"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = "#{Dir.pwd}/vendor:#{Dir.pwd}"
-    system "make", "binary"
-    bin.install "bin/rocker-compose"
+    ENV["GOPATH"] = "#{Dir.pwd}/build"
+    ENV["GO15VENDOREXPERIMENT"] = "1"
+
+    sha = `git rev-parse HEAD 2>/dev/null`
+    t = DateTime.now.new_offset(0).strftime("%Y-%m-%d_%H:%M_GMT")
+    system "mkdir -p build/src/github.com/grammarly"
+    system "ln -s `pwd` build/src/github.com/grammarly/rocker-compose"
+    system [
+      "cd ./build/src/github.com/grammarly/rocker-compose && go install",
+      "-ldflags \"-X main.Version=#{version} -X main.GitCommit=#{sha[0,7]}",
+      "-X main.GitBranch=master -X main.BuildTime=#{t}_Homebrew\""
+    ].join(" ")
+    bin.install "build/bin/rocker-compose"
   end
 
   test do
