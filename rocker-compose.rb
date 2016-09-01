dc8a30e268099177766ac658831945e7d05bd7bb
Rodion Borovyk <rodion.borovyk@gmail.com>
2016-09-01 17:29:34 +0300

    Update rocker-compose.rb

--- rocker-compose.rb
+++ rocker-compose.rb
@@ -1,9 +1,9 @@
 class RockerCompose < Formula
   desc "Docker tool for deploying apps composed of multiple containers."
   homepage "https://github.com/grammarly/rocker-compose"
-  url "https://github.com/grammarly/rocker-compose.git", :tag => "0.1.5"
+  url "https://github.com/grammarly/rocker-compose.git", :tag => "0.1.6"
   head "https://github.com/grammarly/rocker-compose.git"
-  version "0.1.5"
+  version "0.1.6"
 
   depends_on "go" => :build
 
